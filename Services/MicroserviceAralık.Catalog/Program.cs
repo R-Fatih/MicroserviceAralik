1e3b84225764706aa9681736d9259fbd0751f090
tanersaydam <tanersaydam@gmail.com>
2024-12-09 14:18:11 +0300

    up

--- Services/MicroserviceAralık.Catalog/Program.cs
+++ Services/MicroserviceAralık.Catalog/Program.cs
@@ -1,6 +1,9 @@
+using MicroserviceAralýk.Catalog.Configurations;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.RegisterServices(builder.Configuration);
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
