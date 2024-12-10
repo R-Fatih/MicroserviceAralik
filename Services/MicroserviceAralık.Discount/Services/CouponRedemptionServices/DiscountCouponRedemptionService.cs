db373ba9fca595e059887314280dd089356d28de
R-Fatih <arfatihim@gmail.com>
2024-12-10 11:59:59 +0300

    up

---
+++ Services/MicroserviceAralık.Discount/Services/CouponRedemptionServices/DiscountCouponRedemptionService.cs
@@ -0,0 +1,44 @@
+﻿using MicroserviceAralık.Discount.Context;
+using MicroserviceAralık.Discount.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace MicroserviceAralık.Discount.Services.CouponRedemptionServices;
+
+public class DiscountCouponRedemptionService(AppDbContext _context) : IDiscountCouponRedemptionService
+{
+    public async Task<CouponRedemption> CreateCouponRedemption(CouponRedemption couponRedemption)
+    {
+        var result = await _context.CouponRedemptions.AddAsync(couponRedemption);
+        await _context.SaveChangesAsync();
+        return result.Entity;
+
+    }
+
+    public async Task<bool> DeleteCouponRedemption(int id)
+    {
+        var redemption = await _context.CouponRedemptions.FindAsync(id);
+        var result = _context.CouponRedemptions.Remove(redemption);
+        await _context.SaveChangesAsync();
+        return result != null ? true : false;
+
+    }
+
+    public async Task<List<CouponRedemption>> GetAllCouponRedemptions()
+    {
+        return await _context.CouponRedemptions.ToListAsync();
+    }
+
+    public async Task<CouponRedemption> GetCouponRedemptionById(int id)
+    {
+
+        return await _context.CouponRedemptions.FindAsync(id);
+    }
+
+    public async Task<CouponRedemption> UpdateCouponRedemption(CouponRedemption couponRedemption)
+    {
+        var result = _context.CouponRedemptions.Update(couponRedemption);
+        await _context.SaveChangesAsync();
+        return result.Entity;
+
+    }
+}
---
+++ Services/MicroserviceAralık.Discount/Services/CouponRedemptionServices/IDiscountCouponRedemptionService.cs
@@ -0,0 +1,12 @@
+﻿using MicroserviceAralık.Discount.Entities;
+
+namespace MicroserviceAralık.Discount.Services.CouponRedemptionServices;
+
+public interface IDiscountCouponRedemptionService
+{
+    Task<List<CouponRedemption>> GetAllCouponRedemptions();
+    Task<CouponRedemption> GetCouponRedemptionById(int id);
+    Task<CouponRedemption> CreateCouponRedemption(CouponRedemption couponRedemption);
+    Task<CouponRedemption> UpdateCouponRedemption(CouponRedemption couponRedemption);
+    Task<bool> DeleteCouponRedemption(int id);
+}
