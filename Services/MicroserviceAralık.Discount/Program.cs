f1557b660d4e2ca63b6361ce1199542ac3a2ddce
R-Fatih <arfatihim@gmail.com>
2024-12-10 13:52:50 +0300

    up

--- Services/MicroserviceAralık.Discount/Program.cs
+++ Services/MicroserviceAralık.Discount/Program.cs
@@ -1,6 +1,7 @@
 using MicroserviceAralýk.Discount.Context;
 using MicroserviceAralýk.Discount.Mappings;
 using MicroserviceAralýk.Discount.Services;
+using MicroserviceAralýk.Discount.Services.CouponRedemptionServices;
 using MicroserviceAralýk.Discount.Services.CouponServices;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -8,6 +9,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddScoped<IDiscountCouponService, DiscountCouponService>();
+builder.Services.AddScoped<IDiscountCouponRedemptionService, DiscountCouponRedemptionService>();
 builder.Services.AddDbContext<AppDbContext>();
 builder.Services.AddAutoMapper(typeof(GeneralMapping));
 
@@ -17,6 +19,7 @@ var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 app.MapGrpcService<CouponsService>();
+app.MapGrpcService<CouponRedemptionsService>();
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 
 app.Run();
