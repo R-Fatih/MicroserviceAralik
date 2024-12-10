458911ce6da26b09dff64f912fad9db8aeec5a5f
R-Fatih <arfatihim@gmail.com>
2024-12-10 15:06:43 +0300

    up

--- Services/Order/Infrastructure/MicroserviceAralık.Order.Persistence/Context/AppDbContext.cs
+++ Services/Order/Infrastructure/MicroserviceAralık.Order.Persistence/Context/AppDbContext.cs
@@ -7,7 +7,7 @@ public class AppDbContext : DbContext
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
 
-        optionsBuilder.UseSqlServer("Server=localhost,1481;Initial Catalog=OrderDb;user=sa;password=123546aA.;TrustServerCertificate=true;");
+        optionsBuilder.UseSqlServer("Server=localhost,1481;Initial Catalog=OrderDb;user=sa;password=123456aA.;TrustServerCertificate=true;");
     }
     public DbSet<Address> Addresses { get; set; }
     public DbSet<Ordering> Orderings { get; set; }
