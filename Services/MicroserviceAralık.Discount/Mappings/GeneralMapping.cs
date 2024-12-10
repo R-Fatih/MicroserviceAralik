41cdad8aa1f94df770aa6d4292ddc0c3420c2435
R-Fatih <arfatihim@gmail.com>
2024-12-10 11:16:18 +0300

    up

---
+++ Services/MicroserviceAralık.Discount/Mappings/GeneralMapping.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using Google.Protobuf.WellKnownTypes;
+using MicroserviceAralık.Discount.Entities;
+using MicroserviceAralık.Discount.Protos;
+
+namespace MicroserviceAralık.Discount.Mappings;
+
+public class GeneralMapping : Profile
+{
+    public GeneralMapping()
+    {
+        CreateMap<Coupon, GetCouponResponse>().ReverseMap();
+        CreateMap<CouponRedemption, GetCouponRedemptionResponse>().ReverseMap();
+
+        CreateMap<Timestamp, DateTime>().ConvertUsing(t => t == null ? default : t.ToDateTime());
+        CreateMap<DateTime, Timestamp>().ConvertUsing(d => Timestamp.FromDateTime(d.ToUniversalTime()));
+    }
+}
