be0db3d4bf97e423f3178781df0d5ed6b4031137
R-Fatih <arfatihim@gmail.com>
2024-12-12 14:28:38 +0300

    up

---
+++ Services/Cargo/MicroserviceAralık.Cargo.DataAccessLayer/Abstract/ICustomerDal.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MicroserviceAralık.Cargo.DataAccessLayer.Abstract;
+internal interface ICustomerDal
+{
+}
