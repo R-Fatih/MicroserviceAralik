c0ac2f906dee75a1b9826cc54db5df56ef63cf26
R-Fatih <arfatihim@gmail.com>
2024-12-13 12:06:01 +0300

    up

--- Services/Cargo/MicroserviceAralık.Cargo.WebApi/Controllers/CargoDetailsController.cs
+++ Services/Cargo/MicroserviceAralık.Cargo.WebApi/Controllers/CargoDetailsController.cs
@@ -1,4 +1,6 @@
 ﻿using MicroserviceAralık.Cargo.BusinessLayer.Abstract;
+using MicroserviceAralık.Cargo.DtoLayer.Dtos.CargoDetailDtos;
+using MicroserviceAralık.Cargo.EntityLayer.Concrete;
 using Microsoft.AspNetCore.Mvc;
 
 namespace MicroserviceAralık.Cargo.WebApi.Controllers;
@@ -30,6 +32,38 @@ public class CargoDetailsController(ICargoDetailService _cargoDetailService) : C
         var result = await _cargoDetailService.GetReceivedCargoDetailByCustomerId(customerId);
         return Ok(result);
     }
-
+    [HttpPost]
+    public async Task<IActionResult> CreateCargoDetail(CreateCargoDetailDto createCargoDetailDto)
+    {
+        var newCargoDetail = new CargoDetail
+        {
+            Barcode = createCargoDetailDto.Barcode,
+            CompanyId = createCargoDetailDto.CompanyId,
+            ReceiverCustomerId = createCargoDetailDto.ReceiverCustomerId,
+            SenderCustomerId = createCargoDetailDto.SenderCustomerId
+        };
+        await _cargoDetailService.CreateAsync(newCargoDetail);
+        return Ok("Cargo detail created successfully!");
+    }
+    [HttpPut]
+    public async Task<IActionResult> UpdateCargoDetail(UpdateCargoDetailDto updateCargoDetailDto)
+    {
+        var updatedCargoDetail = new CargoDetail
+        {
+            Barcode = updateCargoDetailDto.Barcode,
+            CompanyId = updateCargoDetailDto.CompanyId,
+            ReceiverCustomerId = updateCargoDetailDto.ReceiverCustomerId,
+            SenderCustomerId = updateCargoDetailDto.SenderCustomerId,
+            Id = updateCargoDetailDto.Id
+        };
+        await _cargoDetailService.UpdateAsync(updatedCargoDetail);
+        return Ok("Cargo detail updated succesffully!");
+    }
+    [HttpDelete]
+    public async Task<IActionResult> DeleteCargoDetail(int id)
+    {
+        await _cargoDetailService.DeleteAsync(id);
+        return Ok("Cargo detail deleted successfully!");
+    }
 
 }
