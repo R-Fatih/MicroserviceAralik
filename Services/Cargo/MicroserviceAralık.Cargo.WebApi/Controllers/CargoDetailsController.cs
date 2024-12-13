09d0efb761d63ba1a0487a7e5034da4bb407a627
R-Fatih <arfatihim@gmail.com>
2024-12-13 11:15:11 +0300

    up

---
+++ Services/Cargo/MicroserviceAralık.Cargo.WebApi/Controllers/CargoDetailsController.cs
@@ -0,0 +1,35 @@
+﻿using MicroserviceAralık.Cargo.BusinessLayer.Abstract;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MicroserviceAralık.Cargo.WebApi.Controllers;
+[Route("api/[controller]")]
+[ApiController]
+public class CargoDetailsController(ICargoDetailService _cargoDetailService) : ControllerBase
+{
+    [HttpGet]
+    public async Task<IActionResult> GetAllCargoDetails()
+    {
+        var result = await _cargoDetailService.GetAllAsync();
+        return Ok(result);
+    }
+    [HttpGet("{id}")]
+    public async Task<IActionResult> GetCargoDetailById(int id)
+    {
+        var result = await _cargoDetailService.GetByIdAsync(id);
+        return Ok(result);
+    }
+    [HttpGet("GetSentCargoDetails")]
+    public async Task<IActionResult> GetSentCargoDetails(int customerId)
+    {
+        var result = await _cargoDetailService.GetSentCargoDetailByCustomerId(customerId);
+        return Ok(result);
+    }
+    [HttpGet("GetReceivedCargoDetails")]
+    public async Task<IActionResult> GetReceivedCargoDetails(int customerId)
+    {
+        var result = await _cargoDetailService.GetReceivedCargoDetailByCustomerId(customerId);
+        return Ok(result);
+    }
+
+
+}
--- Services/Cargo/MicroserviceAralık.Cargo.WebApi/Program.cs
+++ Services/Cargo/MicroserviceAralık.Cargo.WebApi/Program.cs
@@ -1,7 +1,10 @@
+using MicroserviceAralýk.Cargo.BusinessLayer.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+builder.Services.RegisterBusinessLayer();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
